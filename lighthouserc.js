7e3cdf2fdaf586a92c614a7806102c0a6f72933c
Allan Cleysson <allancleysson@gmail.com>
2024-08-03 17:57:13 -0300

    ajustes do arquivo lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,33 @@
 module.exports = {
     ci: {
-      collect: {
-        url: ['http://cesar.school'],
-        startServerCommand: 'rails server -e production',
-      },
-      upload: {
-        target: 'temporary-public-storage',
-      },
+        collect: {
+            url: ['https://cesar.school/'],
+            numberOfRuns: 3,
+            settings: {
+                onlyCategories: ['accessibility'],
+                maxWaitForLoad: 45 * 1000,
+                emulatedFormFactor: 'desktop',
+                throtllingMethod: 'simulate',
+                throtlling: {
+                    rttMs: 40,
+                    troughputKbps: 10 * 1024,
+                    cpuSlowdownMultiplier: 1,
+                    requestLatencyMs: 0,
+                    downloadThroughputKbps: 0,
+                    uploadThrouhputKbps: 0
+                }
+            }
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        },
+        assert: {
+            assertions: {
+                'categories:pwa': 'off',
+                'categories:seo': 'off',
+                'categories.best-practices': 'off',
+                'categories.accessibility': ['error', { minScore: 0.8 }]
+            }
+        }
     },
-  };
+};
