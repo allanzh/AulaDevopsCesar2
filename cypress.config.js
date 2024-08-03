4fb338c9579a5124d8e462a6ba5bb487499359d9
Allan Cleysson <allancleysson@gmail.com>
2024-08-03 16:46:31 -0300

    skipping failure test

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     chromeWebSecurity: false,
